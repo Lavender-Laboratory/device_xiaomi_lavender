6ace7f59041aad2f54bf07c12e7f462a8ddbc9bb
Jakub Pawlowski <jpawlowski@google.com>
2021-04-13 11:00:12 +0200

    lavender: Use bluetooth.audio@2.1
    bluetooth.audio@2.1 adds new functionalities required for Bluetooth LE
    Audio development

--- device.mk
+++ device.mk
@@ -27,6 +27,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio@2.1-impl:32 \
     audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
